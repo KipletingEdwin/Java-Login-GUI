d2c0e68e8d9d0390eeab417422980e55337e0f48
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-07-28 21:13:15 +0300

    add three classes

---
+++ src/constants/CommonConstants.java
@@ -0,0 +1,5 @@
+package constants;
+
+public class CommonConstants {
+    public  final static String APP_NAME = "Login App";
+}
---
+++ src/gui/LoginGUI.java
@@ -0,0 +1,12 @@
+package gui;
+
+import constants.CommonConstants;
+
+import javax.swing.*;
+
+public class LoginGUI extends JFrame {
+    public  LoginGUI(){
+        super(CommonConstants.APP_NAME);
+        setSize(CommonConstants.FRAME_SIZE[0], CommonConstants.FRAME_SIZE[1]);
+    }
+}
