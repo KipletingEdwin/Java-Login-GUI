1d5b16e7a3f8c15ce689430bb1c15855235a2c76
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-07-29 08:41:07 +0300

    add addGUIComponent

--- src/gui/LoginGUI.java
+++ src/gui/LoginGUI.java
@@ -7,6 +7,14 @@ import javax.swing.*;
 public class LoginGUI extends JFrame {
     public  LoginGUI(){
         super(CommonConstants.APP_NAME);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
         setSize(CommonConstants.FRAME_SIZE[0], CommonConstants.FRAME_SIZE[1]);
+
+        addGUIComponent();
+    }
+    public  void  addGUIComponent(){
+        SpringLayout springLayout = new SpringLayout();
+
     }
 }
